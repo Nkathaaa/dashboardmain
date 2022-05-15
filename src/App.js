5dcd0d4a540ec9cfaa970d2b8060d192121e7b34
catherinesheilankatha@gmail.com <catherinesheilankatha@gmail.com>
2022-05-15 23:05:54 +0300

    Formik submission and validation added

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@
 import './App.css';
 import Router from "./route.js"
 import { BrowserRouter, Route,Routes, Link,Switch } from 'react-router-dom';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import MainLayout from "./Hoc/mainLayout"
+
+import MainLayout from "./Hoc/mainLayout";
+import Auth from "./components/auth"
 function App() {
   return (
   
     <BrowserRouter>
     <MainLayout>
     <Routes>
-      
-      <Route path="/login" element={<Login />}/>
-      <Route path="/signup" element={<Signup/>}/>
-      <Route path="/" element={<Login />}/>
+  
+      <Route path="/" element={<Auth />}/>
+        
+      <Route path="/auth" element={<Auth />}/>
      
     </Routes>
     </MainLayout>
--- src/components/auth/index.js
+++ src/components/auth/index.js
@@ -4,16 +4,25 @@ import * as Yup from 'yup';
 
 const Auth=(props) =>{
     const formik=useFormik({
-        initialValues:{email:'',password:''},
+        initialValues:{firstName:'',lastName:'',emailAddress:'',password:''},
         validationSchema:Yup.object({
-            email:Yup.string()
-            .required("Sorry the email is required")
-            .email("This is not a  valid emauil"),
-             password:Yup.string()
-             .required("Sorry the password is required")
+            firstName:Yup
+            .string()
+            .required("Sorry the Name is required")
+            .max(12,"Sorry,the name is too long"),
+            lastName:Yup
+            .string(),
+            emailAddress:Yup
+            .string()
+            .email("Enter a valid email address"),
+            password:Yup
+            .string()
+            .required("Sorry the Name is required")  
+            .min(8,"Sorry,the name is too short"),
+            
 
-        }),
-        onSubmit:(values,{resetForm})=>{
+        }), 
+        onSubmit:values=>{
             console.log(values)
 
         }
@@ -21,6 +30,67 @@ const Auth=(props) =>{
   return (
     <>
       <div className="auth_container">
+        
+                    <form onSubmit={formik.handleSubmit}>
+                    <div className="container">
+                        <div className="row mb-4">
+                            <div className="col">
+                            <div className="form-outline">
+                            <label className="form-label" for="form3Example1">First name</label>
+                                <input type="text" id="form3Example1" className="form-control" name="firstName"  {...formik.getFieldProps('firstName')}/>
+                                { formik.errors.firstName && formik.touched.firstName?
+                                  <span>{formik.errors.firstName}</span>
+                                  :null
+
+                                }
+                               
+                            </div>
+                            </div>
+                            <div className="col">
+                            <label className="form-label" for="form3Example2">Last name</label>
+                            <div className="form-outline">
+                                <input type="text" id="form3Example2" className="form-control"  name="lastName"  {...formik.getFieldProps('lastName')}/>
+                                { formik.errors.lastName && formik.touched.lastName?
+                                  <span>{formik.errors.lastName}</span>
+                                  :null
+
+                                }
+                            </div>
+                            </div>
+                        </div>
+
+                        <div className="form-outline mb-4">
+                        <label className="form-label" for="form3Example3">Email address</label>
+                            <input type="email" id="form3Example3" className="form-control"  name="emailAddress"  {...formik.getFieldProps('emailAddress')}/>
+                            { formik.errors.emailAddress && formik.touched.emailAddress?
+                                  <span>{formik.errors.emailAddress}</span>
+                                  :null
+
+                                }          
+                    
+                        </div>
+
+                        <div className="form-outline mb-4">
+                        <label className="form-label" for="form3Example4">Password</label>
+                            <input type="password" id="form3Example4" className="form-control"  name="password" {...formik.getFieldProps('password')} />
+                            { formik.errors.password && formik.touched.password?
+                                  <span>{formik.errors.password}</span>
+                                  :null
+
+                                }
+                        </div>
+
+                  
+                
+
+                    
+                        <button type="submit" className="btn btn-primary btn-block mb-4">Sign up</button>
+                        <button type="submit" className="btn btn-primary btn-block mb-4 ml-2">Register</button>
+                     
+                      </div>  
+                    </form>
+                                
+                    
         </div>
     </>
   )
