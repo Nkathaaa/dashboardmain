ea22fdd55a9d7561880fcb57c7c6aea221d2c544
Wagura Maurice <business@waguramaurice.com>
2022-06-03 19:25:42 +0300

    init authentication preparedness

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,90 +1,106 @@
-import React,{useState,useEffect} from 'react'
-import { Grid,Paper,TextField,Button,Typography,Link } from "@mui/material"
-import {useDispatch,useSelector} from "react-redux"
-import { useNavigate } from 'react-router-dom';
-import { useFormik } from 'formik';   
-import { registerUser } from "../../store/actions/users_actions"
-import * as Yup from 'yup';
-import Avatar from '@mui/material/Avatar';
-import LockIcon from '@mui/icons-material/Lock';
+import React, { useState, useEffect } from "react";
+import {
+  Grid,
+  Paper,
+  TextField,
+  Button,
+  Typography,
+  Link,
+} from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import { signInUser } from "../../store/actions/users_actions";
+import * as Yup from "yup";
+import Avatar from "@mui/material/Avatar";
+import LockIcon from "@mui/icons-material/Lock";
 
+const Login = (props) => {
+  const [register, setRegister] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-const Login=(props) =>{
-   const [register,setRegister]=useState(false)
-    const dispatch=useDispatch()
-    const navigate=useNavigate()
+  const formik = useFormik({
+    initialValues: { email: "", password: "" },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .required("Sorry the email is required")
+        .email("This is not a valid email"),
+      password: Yup.string()
+        .required("Sorry the password is required")
+        .min(8, "Must be 8 digits or more"),
+    }),
+    onSubmit: (values, { resetForm }) => {
+      handleSubmit(values);
+    },
+  });
+  const handleSubmit = (values) => {
+    dispatch(signInUser(values));
+    navigate("/dashboard");
+  };
 
-    const formik=useFormik({
-      initialValues:{ email:'',password:''},
-      validationSchema:Yup.object({
-          email:Yup.string()
-          .required('Sorry the email is required')
-          .email('This is not a valid email'),
-          password:Yup.string()
-          .required('Sorry the password is required')
-          .min(8, 'Must be 8 digits or more')
-      }),
-        onSubmit:(values,{resetForm})=>{  
-            handleSubmit(values) 
-        }
-
-        }); 
-        const handleSubmit = (values) =>{
-          dispatch(registerUser())
-          navigate('/dashboard')
-
-        }
-    
-        const errorHelper = (formik, values) => ({
-          error: formik.errors[values] && formik.touched[values] ? true:false,
-          helperText: formik.errors[values] && formik.touched[values] ? formik.errors[values] : null
-      });
-    const paperStyle={ padding:20,height:'70vh',width:350,margin:"20px auto"}
-    const avatarStyle={backgroundColor:"green"}
+  const errorHelper = (formik, values) => ({
+    error: formik.errors[values] && formik.touched[values] ? true : false,
+    helperText:
+      formik.errors[values] && formik.touched[values]
+        ? formik.errors[values]
+        : null,
+  });
+  const paperStyle = {
+    padding: 20,
+    height: "70vh",
+    width: 350,
+    margin: "20px auto",
+  };
+  const avatarStyle = { backgroundColor: "green" };
   return (
     <>
-     <form className="mt-3" onSubmit={formik.handleSubmit}>
-         <Grid>
-            <Paper style={paperStyle}>
+      <form className="mt-3" onSubmit={formik.handleSubmit}>
+        <Grid>
+          <Paper style={paperStyle}>
             <Grid align="center">
-                <Avatar style={avatarStyle}>
-                    <LockIcon/>
-                </Avatar>
-                <h2>   Sign In </h2>
-             </Grid> 
-             <TextField  
-             TextField id="standard-basic"
-             variant="standard"
-              label="Email" 
-               placeholder="Enter Username"
-              fullWidth required
-                {...formik.getFieldProps('email')}
-                {...errorHelper(formik,'email')}/>
-            
-             <TextField 
-              TextField id="standard-basic"
-               variant="standard"
-                type="password"
-                 label="Password"
-                  placeholder="Enter Username" 
-                  fullWidth required
-                  {...formik.getFieldProps('password')}
-                  {...errorHelper(formik,'password')}/>
-             
-             <Button type="submit" sx={{marginTop:3,backgroundColor:"#3bcf93" ,marginBottom:1}}variant="contained" fullWidth >Login</Button>
-          
-             </Paper>   
-
+              <Avatar style={avatarStyle}>
+                <LockIcon />
+              </Avatar>
+              <h2> Sign In </h2>
             </Grid>
-          
+            <TextField
+              TextField
+              id="standard-basic"
+              variant="standard"
+              label="Email"
+              placeholder="Enter Username"
+              fullWidth
+              required
+              {...formik.getFieldProps("email")}
+              {...errorHelper(formik, "email")}
+            />
 
-            </form>
+            <TextField
+              TextField
+              id="standard-basic"
+              variant="standard"
+              type="password"
+              label="Password"
+              placeholder="Enter Username"
+              fullWidth
+              required
+              {...formik.getFieldProps("password")}
+              {...errorHelper(formik, "password")}
+            />
 
-       
-                                
-                    
-       
+            <Button
+              type="submit"
+              sx={{ marginTop: 3, backgroundColor: "#3bcf93", marginBottom: 1 }}
+              variant="contained"
+              fullWidth
+            >
+              Login
+            </Button>
+          </Paper>
+        </Grid>
+      </form>
     </>
-  )
-}
-export default Login
+  );
+};
+export default Login;
--- src/store/actions/users_actions.js
+++ src/store/actions/users_actions.js
@@ -1,24 +1,30 @@
 import * as users from "./index";
 import axios from "axios";
 import endpoint from "../../services/endpoint";
-import { getAuthHeader,getTokenCookie,removeTokenCookie } from "../../utils/tools"
+import {
+  getAuthHeader,
+  getTokenCookie,
+  removeTokenCookie,
+} from "../../utils/tools";
 
-// axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.headers.post["Content-Type"] = "application/json";
 
 export const registerUser = (values) => {
   return async (dispatch) => {
     try {
-      //const user = await axios.post(``,{
-      //   email: values.email,
-      //    password: values.password
-      // });
-      // console.log(user)
-      //dispatch(users.authUser({data: user.data, auth: true }))
-      console.log(values);
-      
+      const user = await axios.post("http://fundraiser.devops/api/register", {
+        name: values.email,
+        email: values.email,
+        password: values.password,
+        password_confirmation: values.password,
+      });
+      console.log(user);
+      // dispatch(users.authUser({ data: user.data, auth: true }));
+      // console.log(values);
+
       //dispatch the success toast,the users.successGlobal refers to the funtions under the action type
       //ie for users its users-actions
-      dispatch(users.successGlobal("User Succesfully Logged In "));
+      dispatch(users.successGlobal("User registration successfully"));
     } catch (error) {
       dispatch(users.errorGlobal("Error in user registration"));
     }
@@ -26,53 +32,36 @@ export const registerUser = (values) => {
 };
 
 export const signInUser = (values) => {
-  //return async (dispatch) => {
-   // try {
-      // const user=await axios.post(``,{
-      //  email:values.email,
-      //  password:values.password
-      // });
-      // console.log(values)
-      // dispatch(users.signUser({data:user.data,auth:true}))
-
-     // endpoint.get("/sanctum/csrf-cookie").then(() => {
-      //  endpoint
-      //    .post("/api/login", {
-      //      email: values.email,
-      //      password: values.password,
-      //    })
-       //   .then((response) => {
-       //     console.log(response.data);
-      //    });
-     // });
-   // } catch (error) {
-   //   dispatch(users.errorGlobal("This is an error"));
-   // }
-  //};
-//};
-    };
-
-
- export const isAuthUser=()=>{
-   return async(dispatch)=>{
-     try{
-       if(!getTokenCookie){
-         throw new Error();
-       }
-     const user=await axios.get(`/api/userAuth`,getAuthHeader())
-     dispatch(users.isUserAuth({data:user.data,auth:true}))
-     }catch(error){
-       dispatch(users.isUserAuth({data:{},auth:false}))
-
-     }
-
-   }
-
- }   
-
- export const signOut= () => {
   return async (dispatch) => {
-      removeTokenCookie();
-      dispatch(users.signUserOut())
-  }
-}
+    try {
+      const user = await axios.post("http://fundraiser.devops/api/login", {
+        email: values.email,
+        password: values.password,
+      });
+      dispatch(users.signUser({ data: user.data, auth: true }));
+    } catch (error) {
+      dispatch(users.errorGlobal("Unauthorized"));
+    }
+  };
+};
+
+export const isAuthUser = () => {
+  return async (dispatch) => {
+    try {
+      if (!getTokenCookie) {
+        throw new Error();
+      }
+      const user = await axios.get(`/api/userAuth`, getAuthHeader());
+      dispatch(users.isUserAuth({ data: user.data, auth: true }));
+    } catch (error) {
+      dispatch(users.isUserAuth({ data: {}, auth: false }));
+    }
+  };
+};
+
+export const signOut = () => {
+  return async (dispatch) => {
+    removeTokenCookie();
+    dispatch(users.signUserOut());
+  };
+};
