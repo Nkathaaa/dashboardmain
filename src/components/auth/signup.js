c75448663a4e2f03c27a9a43bf8a7619a818b94b
catherinesheilankatha@gmail.com <catherinesheilankatha@gmail.com>
2022-06-04 15:32:30 +0300

    Login api added and functioning well

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React,{useState,useEffect} from 'react'
 import { Grid,Paper,TextField,Button,Typography,Link } from "@mui/material"
 import {useDispatch,useSelector} from "react-redux"
@@ -9,27 +10,63 @@ import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { registerUser } from "../../store/actions/users_actions"
 import Checkbox from '@mui/material/Checkbox';
+=======
+import React, { useState, useEffect } from "react";
+import {
+  Grid,
+  Paper,
+  TextField,
+  Button,
+  Typography,
+  Link,
+} from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import Avatar from "@mui/material/Avatar";
+import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import Checkbox from "@mui/material/Checkbox";
+import { registerUser } from "../../store/actions/users_actions";
+>>>>>>> ea22fdd55a9d7561880fcb57c7c6aea221d2c544
 
-const SignUp=(props) =>{
-    const [register,setRegister]=useState(false)
-    const dispatch=useDispatch()
-    const formik=useFormik({
-        initialValues:{email:'',password:'',phoneNumber:''},
-        validationSchema:Yup.object({
-            firstName:Yup
-            .string()
-            .max(12,"Sorry,the name is too long"),
-            lastName:Yup
-            .string(),
-            email:Yup
-            .string()
-            .email("Enter a valid email address")
-            .required("Sorry the Name is required"),
-            password:Yup
-            .string()
-            .required("Sorry the Name is required")  
-            .min(8,"Sorry,the name is too short"),
+const SignUp = (props) => {
+  const [register, setRegister] = useState(false);
+  const dispatch = useDispatch();
+  const formik = useFormik({
+    initialValues: { email: "", password: "", phoneNumber: "" },
+    validationSchema: Yup.object({
+      firstName: Yup.string().max(12, "Sorry,the name is too long"),
+      lastName: Yup.string(),
+      email: Yup.string()
+        .email("Enter a valid email address")
+        .required("Sorry the Name is required"),
+      password: Yup.string()
+        .required("Sorry the Name is required")
+        .min(8, "Sorry,the name is too short"),
+    }),
+    onSubmit: (values, { resetForm }) => {
+      handleSubmit(values);
+    },
+  });
+  const handleSubmit = (values) => {
+    // console.log(values);
+    dispatch(registerUser(values));
+  };
+  const errorHelper = (formik, values) => ({
+    error: formik.errors[values] && formik.touched[values] ? true : false,
+    helperText:
+      formik.errors[values] && formik.touched[values]
+        ? formik.errors[values]
+        : null,
+  });
+  const paperStyle = { padding: 20, width: 400, margin: "0 auto" };
+  const headerStyle = { margin: 0 };
+  const avatarStyle = { backgroundColor: "green" };
+  const marginTop = { marginTop: 5 };
 
+<<<<<<< HEAD
            
         }),
         onSubmit:(values,{resetForm})=>{  
@@ -111,7 +148,81 @@ const SignUp=(props) =>{
         </Grid>
                     
        
+=======
+  return (
+    <>
+      <Grid>
+        <Paper style={paperStyle}>
+          <Grid align="center">
+            <Avatar style={avatarStyle}>
+              <AddCircleIcon />
+            </Avatar>
+            <h2 style={headerStyle}>Sign Up</h2>
+            <Typography variant="caption" gutterBottom>
+              Please fill this form to create an account !
+            </Typography>
+          </Grid>
+          <form className="mt-3" onSubmit={formik.handleSubmit}>
+            <TextField
+              fullWidth
+              label="Name"
+              variant="standard"
+              placeholder="Enter your name"
+              {...formik.getFieldProps("name")}
+              {...errorHelper(formik, "name")}
+            />
+            <TextField
+              fullWidth
+              label="Email"
+              variant="standard"
+              placeholder="Enter your email"
+              required
+              {...formik.getFieldProps("email")}
+              {...errorHelper(formik, "email")}
+            />
+            <TextField
+              fullWidth
+              label="Phone Number"
+              variant="standard"
+              placeholder="Enter your phone number"
+              required
+              {...formik.getFieldProps("phoneNumber")}
+              {...errorHelper(formik, "phoneNumber")}
+            />
+
+            <TextField
+              fullWidth
+              label="Password"
+              type="password"
+              variant="standard"
+              placeholder="Enter your password"
+              required
+              {...formik.getFieldProps("password")}
+              {...errorHelper(formik, "password")}
+            />
+            <TextField
+              fullWidth
+              label="Confirm Password"
+              variant="standard"
+              type="password"
+              placeholder="Confirm your password"
+              {...formik.getFieldProps("password")}
+              {...errorHelper(formik, "password")}
+            />
+
+            <Button
+              sx={{ m: 2 }}
+              type="submit"
+              variant="contained"
+              color="success"
+            >
+              Sign up
+            </Button>
+          </form>
+        </Paper>
+      </Grid>
+>>>>>>> ea22fdd55a9d7561880fcb57c7c6aea221d2c544
     </>
-  )
-}
-export default SignUp
+  );
+};
+export default SignUp;
--- src/store/actions/users_actions.js
+++ src/store/actions/users_actions.js
@@ -1,13 +1,18 @@
 import * as users from "./index";
 import axios from "axios";
 import endpoint from "../../services/endpoint";
-import { getAuthHeader,getTokenCookie,removeTokenCookie } from "../../utils/tools"
+import {
+  getAuthHeader,
+  getTokenCookie,
+  removeTokenCookie,
+} from "../../utils/tools";
 
-// axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.headers.post["Content-Type"] = "application/json";
 
 export const registerUser = (values) => {
   return async (dispatch) => {
     try {
+<<<<<<< HEAD
       const user = await axios.post(`https://h4h.wezatech.co.ke/api/register`,{
        name:values.name,  
        email: values.email,
@@ -21,6 +26,21 @@ export const registerUser = (values) => {
       //dispatch the success toast,the users.successGlobal refers to the funtions under the action type
       //ie for users its users-actions
       dispatch(users.successGlobal("User Succesfully Registered  "));
+=======
+      const user = await axios.post("http://fundraiser.devops/api/register", {
+        name: values.email,
+        email: values.email,
+        password: values.password,
+        password_confirmation: values.password,
+      });
+      console.log(user);
+      // dispatch(users.authUser({ data: user.data, auth: true }));
+      // console.log(values);
+
+      //dispatch the success toast,the users.successGlobal refers to the funtions under the action type
+      //ie for users its users-actions
+      dispatch(users.successGlobal("User registration successfully"));
+>>>>>>> ea22fdd55a9d7561880fcb57c7c6aea221d2c544
     } catch (error) {
       dispatch(users.errorGlobal("Error in user registration"));
     }
@@ -30,6 +50,7 @@ export const registerUser = (values) => {
 export const signInUser = (values) => {
   return async (dispatch) => {
     try {
+<<<<<<< HEAD
       console.log("we are getting here")
      
        const user=await axios.post(`https://h4h.wezatech.co.ke/api/login`,{
@@ -63,12 +84,36 @@ export const signInUser = (values) => {
      }
 
    }
+=======
+      const user = await axios.post("http://fundraiser.devops/api/login", {
+        email: values.email,
+        password: values.password,
+      });
+      dispatch(users.signUser({ data: user.data, auth: true }));
+    } catch (error) {
+      dispatch(users.errorGlobal("Unauthorized"));
+    }
+  };
+};
+>>>>>>> ea22fdd55a9d7561880fcb57c7c6aea221d2c544
 
- }   
-
- export const signOut= () => {
+export const isAuthUser = () => {
   return async (dispatch) => {
-      removeTokenCookie();
-      dispatch(users.signUserOut())
-  }
-}
+    try {
+      if (!getTokenCookie) {
+        throw new Error();
+      }
+      const user = await axios.get(`/api/userAuth`, getAuthHeader());
+      dispatch(users.isUserAuth({ data: user.data, auth: true }));
+    } catch (error) {
+      dispatch(users.isUserAuth({ data: {}, auth: false }));
+    }
+  };
+};
+
+export const signOut = () => {
+  return async (dispatch) => {
+    removeTokenCookie();
+    dispatch(users.signUserOut());
+  };
+};
